b5cc03712b220c6728ad822519bedebb6a14adf9
ShilCode <2850542@gmail.com>
2023-08-13 20:43:27 +0200

    Herobanner

--- src/components/Herobanner.js
+++ src/components/Herobanner.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
+import { Box, Stack, Typography, Button } from '@mui/material'
+import HeroBanner from '../assets/images/banner.png'
+
 const Herobanner = () => {
   return (
-    <div>Herobanner</div>
+    <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+      <Typography color={"#FF2625"} fontWeight={600} fontSize={"26px"}>
+        Fitness Club
+      </Typography>
+      <Typography fontWeight={700} sx={{fontSize:{lg:"44px",xs:"40px"}}}>
+        Sweat, Smile <br/> Repeat
+        </Typography>
+      <Typography fontSize={"22px"} lineHeight={"35px"} mb={3}>
+        Check out the most effective exercises
+        </Typography>
+
+      <Button variant="contained" color='error' >Explore Excecises</Button>
+
+      <img src={HeroBanner} alt='banner'/>
+
+
+    </Box>
   )
 }
 
